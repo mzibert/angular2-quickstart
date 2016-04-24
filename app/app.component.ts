189be25a38df926816c0f6045bfca980b7d882bd
merri zibert <mzibert@cnm.edu>
2016-04-24 14:45:34 -0600

    fixed some errors

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import {Component} from "angular2/core";
 
 @Component({
 	selector: "my-app",
-	templateUrl: "app.templates/splash-page.html"
+	templateUrl: "app/templates/splash-page.html"
 })
 
 export class AppComponent {} 
